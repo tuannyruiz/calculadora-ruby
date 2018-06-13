25efae6eef4c8bb59f11d343e15c803873f8da09
Tuanny Ruiz <tuannyruiz@gmail.com>
2018-06-13 09:57:46 -0300

    prática de case/while

--- agenda/agenda.rb
+++ agenda/agenda.rb
@@ -3,28 +3,41 @@ def limpa_texto(texto)
 end
 
 def descricao_telefone(tipo_telefone)
-    if tipo_telefone == "C"
+    case tipo_telefone
+    when "C"
         "Comercial"
-    elsif tipo_telefone == "R"
+    when "R"
         "Residencial"
-    elsif tipo_telefone == "M"
+    when "M"
         "Celular"
-    else 
+    else
         "Não identificado"
     end
 end
 
-puts "Entre com o seu nome"
-nome = gets
-puts "Seu nome é #{nome}" #Só dá pra interpolar com aspas duplas 
+def obtem_registro
+    puts "Entre com o seu nome"
+    nome = gets
+    puts "Seu nome é #{nome}" #Só dá pra interpolar com aspas duplas 
 
-puts "Digite o seu número de telefone"
-telefone = gets
-puts "#{limpa_texto(nome)} - #{telefone}"
+    puts "Digite o seu número de telefone"
+    telefone = gets
+    puts "#{limpa_texto(nome)} - #{telefone}"
 
-puts "Entre com o tipo de telefone:"
-puts "Comercial [C], Residencial [R] ou celular [M]"
-tipo_telefone = gets
+    puts "Entre com o tipo de telefone:"
+    puts "Comercial [C], Residencial [R] ou celular [M]"
+    tipo_telefone = gets
 
-puts "#{limpa_texto(nome)} - #{limpa_texto(telefone)} - #{descricao_telefone(limpa_texto(tipo_telefone))}"
-puts "Obrigada por inserir seu número na minha agenda!"
+    puts "#{limpa_texto(nome)} - #{limpa_texto(telefone)} - #{descricao_telefone(limpa_texto(tipo_telefone))}"
+    puts "Obrigada por inserir seu número na minha agenda!"
+end
+
+continua = '1'
+
+while continua == '1'
+    obtem_registro
+    puts "Deseja incluir mais números na agenda?" 
+    puts "[1] Sim" 
+    puts "[0] Não"
+    continua = limpa_texto(gets)
+end
